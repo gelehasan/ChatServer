6fb20e979fd09da78c3cc508b0e0d30725a7fd82
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-08-26 01:20:24 +0200

    Handling multiple clinets using select

--- Server.c
+++ Server.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -11,10 +10,14 @@
 #define MAX_CLIENT 10
 int main(void)
 {
-    int sockfd, newSockfd;
-    struct sockaddr_in address;
+    int sockfd, newSockfd, activity, bytes_read, client_socket[MAX_CLIENT],MAX_SD, current_client;
+    struct sockaddr_in address, client_address;
     int addrLength= sizeof(address);
-
+    fd_set reads_fds;
+    char buffer[255];
+    for(int i=0; i<MAX_CLIENT; i++) {
+        client_socket[i]=0;
+    }
     sockfd=socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0) {
         perror( "Error on establishing socket");
@@ -31,22 +34,87 @@ int main(void)
     }
 
 
-    if(listen(sockfd, MAX_CLIENT)<0) {
+    if(listen(sockfd, 3)<0) {
         perror("error on listnining incoming connection");
         exit(EXIT_FAILURE);
+        close(sockfd);
     };
 
+    while(1) {
+        FD_ZERO(&reads_fds);
+        FD_SET(sockfd,&reads_fds);
+        MAX_SD=sockfd;
 
-    printf("Listning on port %d \n", PORT);
-    newSockfd=accept(sockfd,(struct sockaddr *)&address, (socklen_t *)&addrLength);
+        //Checking if there is already connected client and adding them to the monitoring listttttttttttttttttt
+        for(int i=0; i<MAX_CLIENT; i++) {
+            current_client= client_socket[i];
 
-    if(newSockfd <0) {
-        perror("Error on accepting clients ");
-        exit(EXIT_FAILURE);
+            if(current_client >0) {
+                FD_SET(current_client, &reads_fds);
+            }
+
+            if(current_client > MAX_SD) {
+                MAX_SD=current_client;
+            }
+        }
+
+
+        // Monitiring activities
+        activity= select(MAX_SD+1, &reads_fds, NULL, NULL,NULL);
+
+        if(activity <0  ) {
+            printf("Error on selct");
+        }
+
+
+        // Monitoring incomming calls
+        if(FD_ISSET(sockfd,&reads_fds)) {
+            newSockfd= accept(sockfd,(const struct sockaddr*)&address, sizeof(address));
+            if(newSockfd <0) {
+                perror("Error on accepting new incomming call");
+                exit(EXIT_FAILURE);
+            }
+
+           for(int i=0; i<MAX_CLIENT; i++) {
+               current_client= client_socket[i];
+
+               if(current_client==0) {
+                   current_client=newSockfd;
+                   FD_SET(current_client, &reads_fds);
+                   break;
+               }
+           }
+        }
+
+
+        // Monitoring IO operations
+
+        for(int i=0; i<MAX_CLIENT; i++) {
+            current_client= client_socket[i];
+
+           if(FD_ISSET(current_client,&reads_fds)) {
+
+               bytes_read= read(current_client, buffer, 255);
+
+               //If less than zero it means clients disconnected
+               if(bytes_read <0) {
+
+                   getpeername(current_client,(struct sockadd*)&client_address, (socklen_t *)&addrLength);
+
+                   // inet_ntoa converts IP4 address from binary to into its standard dotted-decimal string representation.,
+                   // ntohs converts from network byte to host byte
+                   printf("Disconnected IP: %s Port %d \n",inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+                   close(current_client);
+                   current_client=0;
+               }else {
+
+                   buffer[bytes_read]="\0"; // Null terminator
+                   send(current_client, buffer, strlen(buffer),0);
+               }
+
+           }
+        }
     }
 
-
-    close(newSockfd);
-    close(sockfd);
     return 0;
 }
